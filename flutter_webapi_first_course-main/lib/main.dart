ac278c151193de3356bbade5574964450f06d5cc
Pablo Roberto <pablomoura2016@gmail.com>
2024-05-13 16:30:55 -0300

    Adicionando a tela de registro do diario

--- flutter_webapi_first_course-main/lib/main.dart
+++ flutter_webapi_first_course-main/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/add_journal_screen/add_jorunal_screen.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
@@ -6,10 +8,9 @@ import 'screens/home_screen/home_screen.dart';
 void main() {
   runApp(const MyApp());
 
-  JournalService service = JournalService();
-  service.register("santosssss!");
+  //JournalService service = JournalService();
+  //service.register("santosssss!");
   //service.get();
-
 }
 
 class MyApp extends StatelessWidget {
@@ -21,22 +22,30 @@ class MyApp extends StatelessWidget {
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          backgroundColor: Colors.black,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
+          primarySwatch: Colors.green,
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            backgroundColor: Colors.black,
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+            actionsIconTheme: IconThemeData(color: Colors.white)
           ),
-        ),
-        textTheme: GoogleFonts.bitterTextTheme()
-      ),
+          textTheme: GoogleFonts.bitterTextTheme()),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
-      initialRoute: "home",
+      initialRoute: "add-journal",
       routes: {
         "home": (context) => const HomeScreen(),
+        "add-journal": (context) => AddJournalScreen(
+              journal: Journal(
+                id: "id",
+                content: "content",
+                createdAt: DateTime.now(),
+                updatedAt: DateTime.now(),
+              ),
+            ),
       },
     );
   }
