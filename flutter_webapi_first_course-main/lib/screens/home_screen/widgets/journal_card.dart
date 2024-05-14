dbbbf3f9e128cf12f8343d7e0d1d43ef977eb2dd
Pablo Roberto <pablomoura2016@gmail.com>
2024-05-14 13:05:12 -0300

    não entendi muito bem de começo mas fé q um dia vou entender

--- flutter_webapi_first_course-main/lib/screens/home_screen/widgets/journal_card.dart
+++ flutter_webapi_first_course-main/lib/screens/home_screen/widgets/journal_card.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webapi_first_course/helpers/weekday.dart';
 import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/main.dart';
+import 'package:uuid/uuid.dart';
 
 class JournalCard extends StatelessWidget {
   final Journal? journal;
   final DateTime showedDate;
-  const JournalCard({Key? key, this.journal, required this.showedDate})
+  final Function refreshFunction;
+
+  const JournalCard({Key? key, this.journal, required this.showedDate, required this.refreshFunction})
       : super(key: key);
 
   @override
@@ -79,7 +83,9 @@ class JournalCard extends StatelessWidget {
       );
     } else {
       return InkWell(
-        onTap: () {},
+        onTap: () {
+          callAddJournalScreen(context);
+        },
         child: Container(
           height: 115,
           alignment: Alignment.center,
@@ -91,5 +97,27 @@ class JournalCard extends StatelessWidget {
         ),
       );
     }
+  }
+
+  callAddJournalScreen(BuildContext context) {
+    Navigator.pushNamed(
+      context,
+      "add-journal",
+      arguments: Journal(
+        id: const Uuid().v1(),
+        content: "",
+        createdAt: showedDate,
+        updatedAt: showedDate,
+      ),
+    ).then((value) {
+      refreshFunction();
+      if (value != null && value == true) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Diário criado com sucesso!"),
+          ),
+        );
+      }
+    });
   }
 }
